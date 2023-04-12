7f988cf6bc985315cb9783dd3407d9d5dbf9f1b6
devadeva138307 <130572692+devadeva138307@users.noreply.github.com>
2023-04-12 20:20:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    let res=document.getElementById("fName").value;
+    console.log("First_Name: ",res);
+    let res1=document.getElementById("mName").value;
+    console.log("Middle_Nmae: ",res1);
+    let res2=document.getElementById("lName").value;
+    console.log("Last_Name: ",res2);
+    let res3=document.getElementById("email").value;
+    console.log("Email: ",res3);
+    let res4=document.getElementById("addr").value;
+    console.log("Address: ",res4);
+}
